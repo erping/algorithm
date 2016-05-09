5ef7f556ca85f6709163656c256641cae5c4ea3a
erping <erpingtang@gmail.com>
2016-05-09 22:41:16 +0800

    add isPalindrome_ano

---
+++ isPalindrome_ano.cpp
@@ -0,0 +1,18 @@
+/**
+*Given a string, determine if it is a palindrome, considering only alphanumeric 
+*characters and ignoring cases.
+*For example,"A man, a plan, a canal: Panama" is a palindrome.
+*"race a car" is not a palindrome.
+*/
+
+
+
+bool isPalindrome(string s) {
+    for (int i = 0, j = s.size() - 1; i < j; i++, j--) { 
+        while (isalnum(s[i]) == false && i < j) i++; 
+        while (isalnum(s[j]) == false && i < j) j--; 
+        if (toupper(s[i]) != toupper(s[j])) return false; 
+    }
+
+    return true;
+}
